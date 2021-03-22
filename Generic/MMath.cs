1e3ed3e6448f2a9b11e1efc2c0c4c2e737227c10
Mikhail Babich <babich-mv@yandex.ru>
2021-03-22 23:59:55 +1000

    MMath: Add Gaussian methods and other math methods

--- Generic/MMath.cs
+++ Generic/MMath.cs
@@ -5,6 +5,7 @@ namespace Library.Generic
 {
     public static class MMath {
         public const float PI = (float)Math.PI;
+        public const float E = (float)Math.E;
 
         public static double GetDistance(Point p1, Point p2) => (p1 - p2).Magnitude;
 
@@ -19,11 +20,36 @@ namespace Library.Generic
 
         public static double GetDistance(Triangle t, Point p) => Math.Min(Math.Min(GetDistance(t.p1, p), GetDistance(t.p2, p)), GetDistance(t.p3, p));
 
-        public static double Dot(Point p1, Point p2) => p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
+        public static float Dot(Point p1, Point p2) => p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
 
         public static float Cos(float d) => (float)Math.Cos(d);
         public static float Sin(float d) => (float)Math.Sin(d);
+        public static float Acos(float d) => (float)Math.Acos(d);
         public static float Round(float d) => (float)Math.Round(d);
+        public static float Sqrt(float x) => (float)Math.Sqrt(x);
+        public static float Pow(float x, float y) => (float)Math.Pow(x, y);
+        public static float Log(float x) => (float)Math.Log(x);
+
+        public static float GetRandomGaussian() {
+            float v1, s;
+            do {
+                v1 = 2.0f * RandomGenerator.Float() - 1.0f;
+                var v2 = 2.0f * RandomGenerator.Float() - 1.0f;
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1.0f || s == 0.0f);
+
+            s = Sqrt(-2.0f * Log(s) / s);
+
+            return v1 * s;
+        }
+
+        public static float GetRandomGaussian(float mean, float standardDeviation) {
+            return mean + GetRandomGaussian() * standardDeviation;
+        }
+
+        public static float GetNormalDistributionProbabilityDensity(float x, float mean, float standardDeviation) {
+            return Pow(E, -Pow(x - mean, 2) / (2 * Pow(standardDeviation, 2))) / (standardDeviation * Sqrt(2 * PI));
+        }
 
         public static double GetDeterminant(List<List<double>> c) {
             if (c.Count > 2) {
