8b9ae2e7c7cb5f89aeba37cece341c0ea767f834
Mikhail Babich <babich-mv@yandex.ru>
2021-07-28 01:36:59 +1000

    Add dictionary utils

---
+++ Generic/DictUtils.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library.Generic
+{
+    public static class DictUtils
+    {
+        public static void FillValueIfNotExists<TK, TV>(Dictionary<TK, TV> dict, TK k, TV defaultValue) {
+            if (!dict.ContainsKey(k)) {
+                dict.Add(k, defaultValue);
+            }
+        }
+
+        public static void SumValue<TK>(Dictionary<TK, double> dict, TK k, double value) {
+            FillValueIfNotExists(dict, k, 0.0);
+            dict[k] += value;
+        }
+    }
+}
